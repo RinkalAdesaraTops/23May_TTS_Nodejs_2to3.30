a39099b76d72cc4c78620de7a69c8664da80fa7c
RinkalAdesaraTops <167286606+RinkalAdesaraTops@users.noreply.github.com>
2024-06-08 15:00:53 +0530

    adminpanel

--- Admin/index.js
+++ Admin/index.js
@@ -3,20 +3,47 @@ const app = express()
 const path = require('path')
 console.log(path.join(__dirname, 'views'));
 app.set('views', path.join(__dirname, 'views'))
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use(express.urlencoded({ extended: true }));
+
 app.set('view engine','ejs')
+let arr = []
 app.get('/',(req,res)=>{
     res.render('home')
 })
-
 app.get('/about',(req,res)=>{
     res.render('about')
 })
+
 
 app.get('/contact',(req,res)=>{
     res.render('contact')
 })
 
-
+app.post('/cat/save',(req,res)=>{
+    let len = arr.length
+    arr.push({
+        id:len+1,
+        name:req.body.catname
+    })
+    console.log(arr);
+    res.redirect('/category')
+})
+app.get('/cat/delete/:id',(req,res)=>{
+   let id = req.params.id;
+//    arr.splice(id-1,1)
+   let data1 =  arr.filter((i)=>{
+        return i.id != id
+    })
+    arr = data1;
+    res.redirect('/category')
+})
+app.get('/category',(req,res)=>{
+    res.render('category',{
+        "catdata":arr
+    })
+})
 app.listen(4000,()=>{
     console.log("Listening on port 4000")
 })
